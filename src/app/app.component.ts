66b0d3b00e4ce703d1bfd63fe6e7cd87689990cd
hamza mandor <hamzabrezy4@gmail.com>
2024-07-17 20:35:56 +0200

    HEADER WTH USERInput

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { UserInputComponent } from "./user-input/user-input.component";
 
-@Component({
+@Component({ 
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, UserInputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'calculator_invest';
 }
+ 
